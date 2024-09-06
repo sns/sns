055bdc63e5513ed5e1bcb9c2352d218614d8c682
Sina <sina.it@gmail.com>
2024-09-06 16:11:07 -0400

    update

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -1,3 +1,4 @@
+import * as trp from '../assets/experience/trp.png';
 import * as qgenda from '../assets/experience/qgenda.png';
 import * as dst from '../assets/experience/dst.png';
 import umbc from '../assets/experience/umbc.svg';
@@ -26,10 +27,31 @@ export const experienceData: IExperienceData = {
     header: 'Experience',
     experiences: [
         {
+            title: 'T.RowePrice',
+            link: 'https://www.troweprice.com/',
+            position: 'Sr. Software Engineer',
+            duration: 'Jun 2020 - Present',
+            details: [
+                "Lead Development Projects: Spearheaded the design and implementation of scalable software solutions for equity ratings, enhancing the accuracy and efficiency of financial analysis tools.",
+                "Technical Leadership: Mentored junior engineers, conducted thorough code reviews, and ensured adherence to best practices in software development, fostering a culture of continuous improvement.",
+                "Performance Optimization: Improved system performance by optimizing algorithms and refactoring legacy code, achieving a 30% reduction in processing time and enhancing overall system efficiency.",
+                "Innovation: Introduced cutting-edge technologies and frameworks, such as microservices architecture, Docker, AWS, and cloud-based solutions, to modernize the equity ratings infrastructure and streamline operations.",
+                "Project Management: Successfully managed multiple projects simultaneously, ensuring timely delivery and alignment with business objectives, while maintaining high standards of quality and performance.",
+                "Full Stack Development: Developed and maintained microservices APIs using Python for backend and TypeScript with React for frontend, ensuring seamless integration and robust functionality.",
+                "CI/CD and DevOps: Implemented and maintained CI/CD pipelines, leveraging Docker and AWS to automate deployment processes and improve development workflows.",
+                "Financial Analysis Tools: Modernized the tools for stock market analyst attribution and performance reporting, performing complex calculations to measure and compare analysts’ performance against various benchmarks.",
+            ],
+            image: {
+                src: trp.default,
+                alt: 'T.RowePrice',
+            },
+            location: 'Baltimore, MD',
+        },        
+        {
             title: 'QGenda',
             link: 'https://www.qgenda.com',
             position: 'Software Engineer',
-            duration: 'Aug 2016 - Present',
+            duration: 'Aug 2016 - May 2020',
             details: [
                 "Using MVC/CQRS architecture, and technologies such as C# .NET, react, redux, and SQL Server to build an automated scheduling platform for physicians",
                 "Working on a scrum team and participating in all aspects of the development process",
